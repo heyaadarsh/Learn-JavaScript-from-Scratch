db6c7ab6ad1fff1b09298ce5745d0e7978052911
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2023-11-19 07:06:28 +0530

    Operations of data types

---
+++ 01_Basics/04_conversionOperations.js
@@ -0,0 +1,40 @@
+// Basic Operations
+
+console.log(10+10); // Addition
+console.log(10-10); // Subtraction
+console.log(10*10); // Multiplication
+console.log(10/10); // Division
+console.log(10%10); // Remainder
+console.log(10**10); // Power; a**b mean a to the power b
+
+let positive = 10;
+let negative = -(positive);
+console.log(negative); // O/P: -10
+
+
+// Adding two strings
+
+let str1 = "Hello";
+let str2 = " World";
+let str3 = str1 + str2; // We can't subtract.
+console.log(str3); // O/P: Hello World
+
+
+// String and integer
+
+console.log("1" + 2); // O/P: 12
+console.log(1 + "2"); // O/P: 12
+console.log("1" + 2 + 3); // O/P: 123
+console.log(1 + 2 + "3"); // O/P: 33
+
+
+console.log(true); // O/P: true
+console.log(-true); // O/P: -1
+console.log(+true); // O/P: 1
+console.log(+""); // O/P: 0; coz "" means false and using operand converts into integer value.
+
+
+// Pre-increment & Post-increment
+// Pre-decrement & Post-decrement
+
+// Notes will be added. You can also contibute :)
