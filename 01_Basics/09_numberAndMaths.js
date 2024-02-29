9b67fc2e5ea8907967d4817e8561dea36e0fa4ec
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2024-02-29 17:54:19 +0530

    Number and Maths in JS

---
+++ 01_Basics/09_numberAndMaths.js
@@ -0,0 +1,47 @@
+// const score = 100;
+// console.log(score); // 100
+
+// const newScore = new Number(100);
+// console.log(newScore); // [Number: 100]
+// console.log(newScore.toString()); // 100 - type: string
+
+// const rate = 256.155723;
+// console.log(rate.toFixed(3));
+
+// const numOne = 76.658;
+// console.log(numOne.toPrecision(3)); // 76.7
+
+// const numTwo = 199.782;
+// console.log(numTwo.toPrecision(3)); // 200
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString()); //10,00,000 - US Standards
+// console.log(hundreds.toLocaleString('en-in')); // 10,00,000 - Indian Standard
+
+
+
+// ++++++++++++++++++++++++ MATHS +++++++++++++++++++++++
+
+console.log(Math.abs(-4)); // 4 : -ve to +ve
+
+console.log(Math.round(4.5)); //5
+console.log(Math.round(4.4)); //4
+
+console.log(Math.floor(5.2)); //5
+console.log(Math.ceil(5.2)); //6
+
+console.log(Math.max(6, 9, 12, -2, 0)); //12: same as min
+
+//.random() gives a value between 0 and 1.
+console.log(Math.random());
+
+/*
+Formula:
+
+const min = 10;
+const max = 50;
+
+console.log(Math.floor() * (max - min + 1) + min);
+
+*/
+
